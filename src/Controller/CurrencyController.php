d1ea1c0658aa4bb3bdf7f6ee9d33bf243cf11ad8
Ivan Kozhukhov <cheremhovo1990@yandex.ru>
2020-03-09 13:32:47 +0300

    add filter

--- src/Controller/CurrencyController.php
+++ src/Controller/CurrencyController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\CurrencyRepository;
+use App\Search\CurrencySearch;
 use Illuminate\Support\Collection;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,20 +14,37 @@ class CurrencyController extends AbstractController
 {
     /**
      * @Route("/", name="currency")
+     * @param CurrencyRepository $currencyRepository
+     * @param PaginatorInterface $paginator
+     * @param Request $request
+     * @param CurrencySearch $currencySearch
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index(CurrencyRepository $currencyRepository, PaginatorInterface $paginator, Request $request)
+    public function index(
+        CurrencyRepository $currencyRepository,
+        PaginatorInterface $paginator,
+        Request $request,
+        CurrencySearch $currencySearch
+    )
     {
-        $sql = $currencyRepository->getQuery();
+
+        $params = $request->query;
+        $sql = $currencySearch->search($params->all());
         $pagination = $paginator->paginate($sql, $request->get('page', 1));
         return $this->render('currency/index.html.twig', [
             'controller_name' => 'CurrencyController',
             'pagination' => $pagination,
+            'dropDownCharCode' => $currencyRepository->getDropDownCharCode(),
+            'params' => $params,
         ]);
     }
 
     /**
      * @Route("/currency/chart", name="currency_chart")
+     * @param Request $request
+     * @param CurrencyRepository $currencyRepository
      * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
      */
     public function chart(Request $request, CurrencyRepository $currencyRepository)
     {
---
+++ src/Search/CurrencySearch.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Search;
+
+
+use App\Repository\CurrencyRepository;
+
+class CurrencySearch
+{
+    /**
+     * @var CurrencyRepository
+     */
+    private CurrencyRepository $currencyRepository;
+
+    public function __construct(CurrencyRepository $currencyRepository)
+    {
+        $this->currencyRepository = $currencyRepository;
+    }
+
+    public function search(array $params)
+    {
+        $sql = $this->currencyRepository->getQuery();
+        if (!empty($params['char_code'])) {
+            $sql->andWhere('c.char_code = :char_code')
+                ->setParameter('char_code', $params['char_code']);
+        }
+        if (!empty($params['datetime'])) {
+            $sql->andWhere('c.datetime = :datetime')
+                ->setParameter('datetime', $params['datetime']);
+        }
+        return $sql;
+    }
+}
